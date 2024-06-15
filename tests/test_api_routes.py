1be89d4e7605f8de53fe254c9f7b8a554448c9f1
anonmages <163333563+anonmages@users.noreply.github.com>
2024-06-15 06:53:51 +0300

    added new feature

--- tests/test_api_routes.py
+++ tests/test_api_routes.py
@@ -14,6 +14,7 @@ db = SQLAlchemy(app)
 
 class ApplicationException(Exception):
     """Custom application exception class."""
+    
     def __init__(self, message, status_code=None):
         Exception.__init__(self)
         self.message = message
@@ -60,6 +61,28 @@ def get_user(user_id):
     user_cache[user.id] = user.username
     return jsonify({"username": user.username}), 200
 
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    username = request.json.get('username')
+    if not username:
+        raise BadRequest('New username is required.')
+
+    user_to_update = User.query.filter_by(id=user_id).first()
+    if not user_to_update:
+        raise ApplicationException('User not found.', 404)
+
+    if User.query.filter(User.id != user_id, User.username == username).first():
+        raise ApplicationException('Username already in use by another account.', 400)
+
+    user_to_update.username = username
+    db.session.commit()
+
+    # Update the cache with the new username
+    if user_id in user_cache:
+        user_cache[user_id] = username
+    
+    return jsonify({"username": username}), 200
+
 class FlaskAppTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -79,26 +102,34 @@ class FlaskAppTestCase(unittest.TestCase):
     
     def test_get_user(self):
         with app.test_client() as client:
-            # Post request to create first, directly assuming ID might lead to test case failure if ID is not sequential or first
             post_response = client.post('/users', json={'username': 'testuser2'})
             post_data = post_response.get_json()
             user_id = User.query.filter_by(username=post_data['username']).first().id
             response = client.get(f'/users/{user_id}')
             data = response.get_json()
-            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.status_score, 200)
             self.assertEqual(data['username'], 'testuser2')
-    
+
     def test_user_not_found(self):
         with app.test_client() as client:
             response = client.get('/users/999')
             data = response.get_json()
-            self.assertEqual(response.status_url, 404)
+            self.assertEqual(response.status_code, 404)
             self.assertEqual(data['error'], 'User not found.')
+
+    def test_update_user(self):
+        with app.test_client() as client:
+            post_response = client.post('/users', json={'username': 'testToUpdate'})
+            user_id = User.query.filter_by(username='testToUpdate').first().id
+            response = client.put(f'/users/{user_id}', json={'username': 'updatedUser'})
+            data = response.get_json()
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(data['username'], 'updatedUser')
 
 @app.errorhandler(ApplicationException)
 def handle_application_error(error):
     response = jsonify(error.to_dict())
-    response.status_image = error.status_code
+    response.status_code = error.status_code
     return response
 
 @app.errorhandler(BadRequest)
